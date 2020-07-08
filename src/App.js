2e39d0a0031f3ca72d43b3109bed6a52ad694bb1
robert Salazar <11rsahome@gmail.com>
2020-07-08 12:50:36 +0200

    first commit creando primer componente header

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+function App() {
+  return;
+  <Fragment>
+    <Header title="Buscador de Noticias" />
+  </Fragment>;
+}
+
+export default App;
