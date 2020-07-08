5ee35d62509924a1557de764cc7aee92bd3e94e5
robert Salazar <11rsahome@gmail.com>
2020-07-08 13:09:15 +0200

    formulario y module Css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import styles from "./Form.module.css";
 
 const Form = () => {
   return (
-    <div className="row">
+    <div className={`${styles.buscador} row`}>
       <div className="col s12 m8 offset-m2">
         <form>
-          {/* <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2> */}
+          <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
           <div className="input-field col s12">
             <input
               type="submit"
-              // className={`${styles["btn-block"]}  btn-large amber darken-2`}
+              className={`${styles["btn-block"]}  btn-large amber darken-2`}
               value="Buscar"
             />
           </div>
